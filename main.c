f1c660fe1d9524479560c7f534fb4db71eaf76e0
ytlan <ytlan0201@gmail.com>
2019-11-16 11:42:16 +0800

    insert and select

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ typedef struct
     char username[COLUMN_USERNAME_SIZE];
     char email[COLUMN_EMAIL_SIZE];
 } Row;
+
 typedef struct
 {
     char* buffer;
@@ -103,20 +104,126 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-void execute_statement(Statement *statement) {
+#define size_of_attribute(Struct, Attribute) sizeof( ((Struct*)0)->Attribute )
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE; 
+
+#define PAGE_SIZE 4096
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct
+{
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
+void serialize_row(Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination) {
+    memcpy(&(destination->id), source+ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source+USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source+EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if (page == NULL) {
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
+typedef enum
+{
+    EXECUTE_SUCCESS,
+    EXECUTE_ERROR
+} ExecuteResult;
+
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+    if( table->num_rows >= TABLE_MAX_ROWS) {
+        return EXECUTE_ERROR;
+    }
+    Row* row_to_insert = &(statement->row_to_insert);
+    void* address = row_slot(table, table->num_rows);
+    serialize_row(row_to_insert, address);
+    table->num_rows += 1;
+    return EXECUTE_SUCCESS;
+}
+
+void print_row(Row* row) {
+    printf("(%d %s %s)\n", row->id, row->username, row->email);
+}
+
+ExecuteResult execute_select(Statement* statement, Table* table) {
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; i++) {
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+
+    return EXECUTE_SUCCESS;
+}
+
+Table* new_table() {
+    Table* table = malloc(sizeof(Table));
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_ROWS; i ++) {
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+void free_table(Table* table) {
+    for (uint32_t i = 0; i < TABLE_MAX_ROWS; i ++) {
+        free(table->pages);
+    }
+    free(table);
+}
+
+void execute_statement(Statement* statement, Table* table) {
     switch(statement->type) {
         case (STATEMENT_INSERT):
             printf("This is where we would do an insert.\n");
             printf("> insert %d %s %s\n", statement->row_to_insert.id, statement->row_to_insert.username, statement->row_to_insert.email);
+            execute_insert(statement, table);
             break;
         case (STATEMENT_SELECT):
             printf("This is where we would do a select.\n");
+            execute_select(statement, table);
             break;
     }
 }
 
+
+
 int main(int argc, char* argv[]) {
+    Table* table = new_table();
+
     InputBuffer* input_buffer = new_input_buffer();
+
+    printf("--- row size --- \n");
+    printf("--- %d\n", ID_SIZE);
+    printf("--- %d\n", USERNAME_SIZE);
+    printf("--- %d\n", EMAIL_SIZE);
+    printf("--- %lu\n", sizeof(int));
+    printf("--- %lu\n", sizeof(char));
+    printf("--- %lu\n", sizeof(Row));
+    printf("--- %u\n", ROW_SIZE);
 
     while (true) {
         print_prompt();
@@ -143,7 +250,7 @@ int main(int argc, char* argv[]) {
                 break;
         }
 
-        execute_statement(&statement);
+        execute_statement(&statement, table);
         printf("Executed.\n");
     }
 }
