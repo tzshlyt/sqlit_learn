b35bee3ab0695cd1d6df6e184cc7e35582095239
ytlan <ytlan0201@gmail.com>
2019-11-19 19:14:28 +0800

    复制字符串前初始化内存

--- main.c
+++ main.c
@@ -166,8 +166,8 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
 void serialize_row(Row* source, void* destination)
 {
     memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    strncpy(destination + USERNAME_OFFSET, source->username, USERNAME_SIZE);
+    strncpy(destination + EMAIL_OFFSET, source->email, EMAIL_SIZE);
 }
 
 void deserialize_row(void* source, Row* destination)
